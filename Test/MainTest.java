ea2bf9b22b2f79c43649fc2e5e4f8f945e91907a
George Sydby <george.sydby@gmail.com>
2019-05-16 14:57:17 +0200

    Uppdaterar MainTest

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -1,12 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void main() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void addproduct() {
+    }
+
+    @Test
     void getID() {
     }
 }
