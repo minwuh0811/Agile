31f4fdb5e2757c56dc3c2975470a65bc19d79f45
Krasen Parvanov <krasenparvanov@gmail.com>
2019-05-16 10:15:45 +0200

    Administrator Class

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,2 +1,84 @@
+import java.util.ArrayList;
+
 public class Administrator {
+
+    /*
+    The Administrator Class maneges the administrator and their getters and setters
+*/
+
+    private String administratorFirstName;
+    private String administratorLastName;
+    private String staffNumberID;
+    private int administratorPhone;
+    private String loginName;
+    private String loginPassword;
+
+    public Administrator (String administratorFirstName, String administratorLastName, String staffNumberID, int administratorPhone, String loginName, String loginPassword) {
+        this.administratorFirstName = administratorFirstName;
+        this.administratorLastName = administratorLastName;
+        this.staffNumberID = staffNumberID;
+        this.administratorPhone = administratorPhone;
+        this.loginName = loginName;
+        this.loginPassword = loginPassword;
+    }
+
+    @Override
+    public String toString () {
+        return "Administrator{" +
+                "administratorFirstName='" + administratorFirstName + '\'' +
+                ", administratorLastName='" + administratorLastName + '\'' +
+                ", staffNumberID='" + staffNumberID + '\'' +
+                ", administratorPhone=" + administratorPhone +
+                ", loginName='" + loginName + '\'' +
+                ", loginPassword='" + loginPassword + '\'' +
+                '}';
+    }
+
+    public String getAdministratorFirstName () {
+        return administratorFirstName;
+    }
+
+    public void setAdministratorFirstName (String administratorFirstName) {
+        this.administratorFirstName = administratorFirstName;
+    }
+
+    public String getAdministratorLastName () {
+        return administratorLastName;
+    }
+
+    public void setAdministratorLastName (String administratorLastName) {
+        this.administratorLastName = administratorLastName;
+    }
+
+    public String getStaffNumberID () {
+        return staffNumberID;
+    }
+
+    public void setStaffNumberID (String staffNumberID) {
+        this.staffNumberID = staffNumberID;
+    }
+
+    public int getAdministratorPhone () {
+        return administratorPhone;
+    }
+
+    public void setAdministratorPhone (int administratorPhone) {
+        this.administratorPhone = administratorPhone;
+    }
+
+    public String getLoginName () {
+        return loginName;
+    }
+
+    public void setLoginName (String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getLoginPassword () {
+        return loginPassword;
+    }
+
+    public void setLoginPassword (String loginPassword) {
+        this.loginPassword = loginPassword;
+    }
 }
